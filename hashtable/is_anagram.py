dcbf57d345616aa0b0de2e7fac578e698c065d4e
samandar <samandar@samandarui-MacBookAir.local>
2023-02-07 13:08:56 +0900

    valid_brackets

---
+++ hashtable/is_anagram.py
@@ -0,0 +1,24 @@
+"""
+Testing:
+is_anagram("lama", "alam") => True
+is_anagram("mosh", "shom") => True
+is_anagram("non", "nok") => False
+"""
+
+def is_anagram(word1: str, word2: str) -> bool:
+    newDict = {}
+    for i in word1:
+        if i in newDict:
+            newDict[i] += 1
+        else:
+            newDict[i] = 1
+    for k in word2:
+        if k in newDict and newDict[k] != 0:
+            newDict[k] -= 1
+        else:
+            return False
+    return True
+
+assert is_anagram("lama", "alam") == True
+assert is_anagram("mosh", "shom") == True
+assert is_anagram("non", "nok") == False
