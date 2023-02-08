df66b92e897921508fdf1fcb947a992df8ad5ecc
samandar <samandar@samandarui-MacBookAir.local>
2023-02-08 09:06:43 +0900

    solved

---
+++ hashtable/two_sum.py
@@ -0,0 +1,14 @@
+"""
+For example :
+two_sum([1,4,5,2], 3) => True # as 1 + 2 are present in the list, 1 + 2 = 3
+two_sum([1,4,5,2], 8) => False
+"""
+def two_sum(arr: list[int], target: int) -> bool:
+    complements = {}
+    for i in arr:
+        subsc = target - i
+        if subsc in complements:
+            return True
+        else:
+            complements[i] = subsc
+    return False
