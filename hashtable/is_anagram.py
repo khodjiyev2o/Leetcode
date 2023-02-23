74cc7fe0b71546837e3c48711390597f86f40cc3
samandar <samandar@samandarui-MacBookAir.local>
2023-02-23 10:02:30 +0900

    is_anagram 2nd way

--- hashtable/is_anagram.py
+++ hashtable/is_anagram.py
@@ -19,6 +19,23 @@ def is_anagram(word1: str, word2: str) -> bool:
             return False
     return True
 
+def isAnagram(word1: str, word2: str) -> bool:
+        newDict = {}
+        for i in word1:
+            if i in newDict:
+                newDict[i] += 1
+            else:
+                newDict[i] = 1
+        for i in word2:
+            if i not in newDict or newDict[i] <= 0:
+                return False
+        return True
+
 assert is_anagram("lama", "alam") == True
 assert is_anagram("mosh", "shom") == True
 assert is_anagram("non", "nok") == False
+
+
+assert isAnagram("lama", "alam") == True
+assert isAnagram("mosh", "shom") == True
+assert isAnagram("non", "nok") == False
---
+++ hashtable/unique_morse.py
@@ -0,0 +1,12 @@
+from typing import List
+
+def uniqueMorseRepresentations(words: List[str]) -> int:
+    characters = [
+        ".-","-...","-.-.","-..",".","..-.","--.","....","..",
+        ".---","-.-",".-..","--","-.","---",".--.","--.-",".-.",
+        "...","-","..-","...-",".--","-..-","-.--","--.."]
+    
+    pass
+
+
+assert uniqueMorseRepresentations(["gin","zen","gig","msg"]) == 2
