bf8f5e3a90833d3bc09cfeea186fcc1bd5d30c7c
samandar <samandarkhodjiyev@gmail.com>
2023-02-23 20:42:04 +0900

    group anagrams

--- hashtable/group_anagrams.py
+++ hashtable/group_anagrams.py
@@ -17,13 +17,26 @@ def groupAnagrams(strs: List[str]) -> List[List[str]]:
         if not newDict or to_add:
             newDict[count] = counter
             result.append([word])
-            count += 1
-            
+            count += 1      
     return result
 
-
+def groupAnagrams2(strs: List[str]) -> List[List[str]]:
+    result = {}
+    for word in strs:
+        chars = ''.join(sorted(word))
+        if chars in result:
+            result[chars].append(word)
+        else:
+            result[chars] = [word]
+    return [x for x in result.values()]
 
 assert groupAnagrams(strs=["eat","tea","tan","ate","nat","bat"]) == [['eat', 'tea', 'ate'], ['tan', 'nat'], ['bat']]
 assert groupAnagrams(strs = [""]) == [[""]]
 assert groupAnagrams(strs = ["a"]) == [["a"]]
 assert groupAnagrams(strs=["samandar","samandar","radnamas","qodir","ridoq","ravnaq"]) == [['samandar', 'samandar', 'radnamas'], ['qodir', 'ridoq'], ['ravnaq']]
+
+
+assert groupAnagrams2(strs=["samandar","samandar","radnamas","qodir","ridoq","ravnaq"]) == [['samandar', 'samandar', 'radnamas'], ['qodir', 'ridoq'], ['ravnaq']]
+assert groupAnagrams2(strs=["eat","tea","tan","ate","nat","bat"]) == [['eat', 'tea', 'ate'], ['tan', 'nat'], ['bat']]
+assert groupAnagrams2(strs = [""]) == [[""]]
+assert groupAnagrams2(strs = ["a"]) == [["a"]]
