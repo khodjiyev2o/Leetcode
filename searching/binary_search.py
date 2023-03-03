0a060b9c2cbcaad1fa98286acf2e7ecf51bad279
samandar <samandarkhodjiyev@gmail.com>
2023-03-03 23:24:16 +0500

    binary search 2nd solution

--- searching/binary_search.py
+++ searching/binary_search.py
@@ -1,3 +1,4 @@
+import random
 def search(nums: list[int], target: int) -> list[int]:
     left = 0
     right = len(nums) - 1
@@ -18,4 +19,26 @@ assert search(nums=nums, target=5) == 3
 assert search(nums=nums, target=9) == 4
 assert search(nums=nums, target=0) == 1
 assert search(nums=nums, target=12) == 5
+
+
+def isBadVersion(num: int) -> bool:
+    if num <= 0:
+        return False
+    arr = [False, False, False, True, True]
+    return arr[num - 1]
+
+def firstBadVersion(n):
+    left, right = 1, n
+    while left < right:
+        mid = (left + right) // 2
+        if isBadVersion(mid):
+            right = mid
+        else:
+            left = mid + 1
+    return left
+
+assert firstBadVersion(n=5) == 4
+assert firstBadVersion(n=4) == 4
+assert firstBadVersion(n=6) == 4
+
 
